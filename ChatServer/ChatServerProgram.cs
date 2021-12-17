5ffc537dd762d8656b4ecac33ca9f4a0f6a0b9ea
seokhwanchoi <bmsk20@gmail.com>
2021-12-17 18:54:14 +0900

    초기 커밋.

---
+++ ChatServer/ChatServerProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatServer
+{
+    class ChatServerProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("서버콘솔창 \n\n\n");
+        }
+    }
+}
