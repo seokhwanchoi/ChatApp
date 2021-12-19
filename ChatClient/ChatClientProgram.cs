d3f46efed0f9c811095fb5b6608014cf706fa896
seokhwanchoi <bmsk20@gmail.com>
2021-12-19 23:50:50 +0900

    reflection attribute 특성 중복 - csproj의 GenerateAssemblyInfo 추가해줘도 안됨

--- ChatClient/ChatClientProgram.cs
+++ ChatClient/ChatClientProgram.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ChatClient
 {
     class ChatClientProgram
     {
+        TcpClient client = null;
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.WriteLine("==========클라이언트==========");
+                Console.WriteLine("1.서버연결");
+                Console.WriteLine("2.Message 보내기");
+                Console.WriteLine("===============================");
+
+                string key = Console.ReadLine();
+                int order = 0;
+
+                if (int.TryParse(key, out order))
+                {
+                    switch (order)
+                    {
+                        case 1:
+                            {
+                                if (client != null)
+                                {
+                                    Console.WriteLine("이미 연결되어있습니다.");
+                                    Console.ReadKey();
+                                }
+                                else
+                                {
+                                    Connect();
+                                }
+
+                                break;
+
+                            }
+                        case 2:
+                            {
+                                if (client == null)
+                                {
+                                    Console.WriteLine("먼저 서버와 연결해주세요");
+                                    Console.ReadKey();
+                                }
+                                else
+                                {
+                                    SendMessage();
+                                }
+                                break;
+                            }
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("잘못 입력하셨습니다.");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+
+            }
+        }
+
+        private void SendMessage()
+        {
+            Console.WriteLine("보낼 message를 입력해주세요");
+            string message = Console.ReadLine();
+            byte[] byteData = new byte[message.Length];
+            byteData = Encoding.Default.GetBytes(message);
+
+            client.GetStream().Write(byteData, 0, byteData.Length);
+            // reference : https://docs.microsoft.com/ko-kr/dotnet/api/system.net.sockets.networkstream.write?view=net-6.0#System_Net_Sockets_NetworkStream_Write_System_Byte___System_Int32_System_Int32_
+
+            Console.WriteLine("전송성공");
+            Console.ReadKey();
+        }
+
+
+        private void Connect()
+        {
+            client = new TcpClient();
+            client.Connect("127.0.0.1", 9999);
+            //client.Connect("127.0.0.2", 9999);
+            Console.WriteLine("서버연결 성공 이제 Message를 입력해주세요");
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChatClientProgram ccp = new ChatClientProgram();
+            ccp.Run();
         }
     }
 }
--- ChatServer/ChatServerProgram.cs
+++ ChatServer/ChatServerProgram.cs
@@ -1,12 +1,84 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace ChatServer
 {
     class ChatServerProgram
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("서버콘솔창 \n\n\n");
+            MyServer a = new MyServer();
+        
         }
+    }
+    class MyServer
+    {
+        public MyServer()
+        {
+            AsyncServerStart();
+        }
+
+        private void AsyncServerStart()
+        {
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Any, 9999));
+            listener.Start();
+            Console.WriteLine("서버를 시작합니다. 클라이언트의 접속을 기다립니다.");
+
+
+            
+            
+
+            while (true)
+            {
+                TcpClient acceptClient = listener.AcceptTcpClient();
+                ClientData clientData = new ClientData(acceptClient);
+                clientData.client.GetStream().BeginRead(clientData.readByteData, 0, clientData.readByteData.Length, new AsyncCallback(DataReceived), clientData);
+                //BeginInvoke, BeginRead, BeginWrite 등 앞에 Begin이 붙은 메서드들은 비동기입니다.
+            }
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            //콜백 메서드란 다른 함수에 인수로 전달되는 함수이며, 이벤트 후에 실행되는 것
+
+            ClientData callbackClient = ar.AsyncState as ClientData;
+
+            int bytesRead = callbackClient.client.GetStream().EndRead(ar);
+
+            string readString = Encoding.Default.GetString(callbackClient.readByteData, 0, bytesRead);
+
+            Console.WriteLine("{0}번 사용자 : {1}", callbackClient.clientNumber, readString);
+
+            //중요
+            callbackClient.client.GetStream().BeginRead(callbackClient.readByteData, 0, callbackClient.readByteData.Length, new AsyncCallback(DataReceived), callbackClient);
+
+
+        }
+    }
+
+    class ClientData
+    { 
+        public TcpClient client { get; set; }
+        public byte[] readByteData { get; set; }
+
+        public int clientNumber;
+
+        public ClientData(TcpClient client)
+        {
+            this.client = client;
+            this.readByteData = new byte[1024];
+
+            string clientEndPoint = client.Client.LocalEndPoint.ToString();
+            char[] point = { '.', ':' };
+            string[] splitedData = clientEndPoint.Split(point);
+            this.clientNumber = int.Parse(splitedData[3]);
+            Console.WriteLine("{0}번 사용자 접속성공", clientNumber);
+
+        }
+    
     }
 }
